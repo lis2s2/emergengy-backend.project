6012b2e8ce7dbc66f026ded267a37425e1a59998
David <davidkang107@gmail.com>
2024-06-25 09:56:46 +0900

    6/25 자바스프링 이니셜라이저 업데이트

---
+++ src/test/java/project/emergency/EmergencyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.emergency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
